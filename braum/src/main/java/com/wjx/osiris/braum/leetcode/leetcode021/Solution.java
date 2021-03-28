987bdd3f11d29466babc930280f4cd3a63353c32
流沙 <liusha@wacai.com>
2021-03-28 12:24:19 +0800

    leetcode21

---
+++ braum/src/main/java/com/wjx/osiris/braum/leetcode/leetcode021/Solution.java
@@ -0,0 +1,41 @@
+package com.wjx.osiris.braum.leetcode.leetcode021;
+
+/**
+ * @author liusha
+ * @date 2021/3/28 12:19
+ * @mail liusha@wacai.com
+ * @description
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ braum/src/main/java/com/wjx/osiris/braum/leetcode/leetcode021/idea.md
@@ -0,0 +1,37 @@
+```java
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+```
+
+首先我们可以考虑空串的问题，两边只要有任意一个出现空串返回另一个即可。否则，我们要判断 l1 和 l2 哪一个链表的头节点的值更小，然后递归地决定下一个添加到结果里的节点。如果两个链表有一个为空，递归结束。
