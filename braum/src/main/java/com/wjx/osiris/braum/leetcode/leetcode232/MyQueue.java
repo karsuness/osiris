3406c03e9c9eda0db317e94617d21242bcbd4626
流沙 <liusha@wacai.com>
2021-03-30 23:20:48 +0800

    leetcode232

---
+++ braum/src/main/java/com/wjx/osiris/braum/leetcode/leetcode232/MyQueue.java
@@ -0,0 +1,51 @@
+package com.wjx.osiris.braum.leetcode.leetcode232;
+
+import java.util.Stack;
+
+/**
+ * @author liusha
+ * @date 2021/3/30 23:13
+ * @mail liusha@wacai.com
+ * @description
+ */
+public class MyQueue {
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
---
+++ braum/src/main/java/com/wjx/osiris/braum/leetcode/leetcode232/idea.md
@@ -0,0 +1,44 @@
+```java
+public class MyQueue {
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+```
+这里我设置两个栈，一个是进栈，一个是出栈，每次push的值都放到进栈中，当调用pop或者peek的时候判断一下出栈是否为空，当不为空时直接操作出栈，如果为空将进栈的数据全部放入出栈再操作
