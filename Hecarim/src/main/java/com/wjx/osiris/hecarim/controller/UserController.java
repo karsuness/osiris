833a4ff91ddab9d7cf5a2dc99613633990b5e279
流沙 <liusha@wacai.com>
2022-05-16 14:06:28 +0800

    测试controller

--- Hecarim/src/main/java/com/wjx/osiris/hecarim/controller/UserController.java
+++ Hecarim/src/main/java/com/wjx/osiris/hecarim/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.wjx.osiris.hecarim.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wjx.osiris.hecarim.domain.User;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author liusha
@@ -17,5 +16,11 @@ public class UserController {
     @GetMapping("/list")
     public String list() {
         return "testxzzz";
+    }
+
+    @PostMapping("/query")
+    public String query(@RequestBody User user) {
+        System.out.println(user);
+        return "String Post";
     }
 }
